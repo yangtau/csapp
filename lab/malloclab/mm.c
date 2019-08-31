7ce9538e62957d7ed2d0410e8b30a18b9cd00562
yangtau <yangtaojay@gmail.com>
2019-08-31 22:09:15 +0800

    beat fit

--- lab/malloclab/mm.c
+++ lab/malloclab/mm.c
@@ -53,13 +53,14 @@ free block
 
 // #define DEBUG
 #define VERBOSE 1
+#define BEST_FIT  // best fit
 
 #ifdef DEBUG
 #define DEBUG_RETURN(args)                              \
-    {                                                   \
+    do {                                                \
         mm_check_heap(__FUNCTION__, __LINE__, VERBOSE); \
         return args;                                    \
-    }
+    } while (0)
 #else  // DEBUG
 #define DEBUG_RETURN(args) \
     { return args; }
@@ -74,7 +75,7 @@ free block
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 /* Size of pointer */
-#define PTR_SIZE (ALIGN(sizeof(char *)))
+#define PTR_SIZE (ALIGN(sizeof(void *)))
 
 #define MINIMUM_BLOCK_SIZE (SIZE_T_SIZE * 2 + PTR_SIZE * 2)
 
@@ -114,13 +115,15 @@ free block
     (PUT((p), size_t, (GET((p), size_t) & ~0x2) | ((val) << 1)))
 
 /* Given block pointer bp, compute address of its header, footer*/
-#define GET_HEAD(bp) ((char *)(bp)-SIZE_T_SIZE)
-#define GET_FOOT(bp) ((char *)(bp) + GET_SIZE(GET_HEAD(bp)) - 2 * SIZE_T_SIZE)
+#define GET_HEAD(bp) ((void *)((char *)(bp)-SIZE_T_SIZE))
+#define GET_FOOT(bp) \
+    ((void *)((char *)(bp) + GET_SIZE(GET_HEAD(bp)) - 2 * SIZE_T_SIZE))
 
 /* Given block ptr bp, compute the address of next block */
-#define GET_NEXT(bp) ((char *)(bp) + GET_SIZE(GET_HEAD(bp)))
+#define GET_NEXT(bp) ((void *)((char *)(bp) + GET_SIZE(GET_HEAD(bp))))
 /* Given block ptr bp, compute the address of previous block */
-#define GET_PREV(bp) ((char *)(bp)-GET_SIZE((char *)(bp)-2 * SIZE_T_SIZE))
+#define GET_PREV(bp) \
+    ((void *)((char *)(bp)-GET_SIZE((char *)(bp)-2 * SIZE_T_SIZE)))
 
 /*
  * NOTE:
@@ -130,77 +133,44 @@ free block
  */
 
 /* Given block ptr bp, compute the address of predecessor and successor */
-#define GET_PRED(bp) (*(char **)(bp))
-#define GET_SUCC(bp) (*(char **)((char *)(bp) + PTR_SIZE))
+#define GET_PRED(bp) (*(void **)(bp))
+#define GET_SUCC(bp) (*(void **)((char *)(bp) + PTR_SIZE))
 
 /* Given block ptr bp, compute the address of address predecessor */
-#define GET_PRED_P(bp) ((char *)(bp))
+#define GET_PRED_P(bp) ((void *)(bp))
 /* Given block ptr bp, compute the address of address successor */
-#define GET_SUCC_P(bp) ((char *)(bp) + PTR_SIZE)
+#define GET_SUCC_P(bp) ((void *)((char *)(bp) + PTR_SIZE))
 
 /* Given header ptr hp, compute the address of block*/
-#define GET_BLOCK(hp) ((char *)(hp) + SIZE_T_SIZE)
+#define GET_BLOCK(hp) ((void *)((char *)(hp) + SIZE_T_SIZE))
 
 static char *mm_heap_start = NULL;
 
-/*
- * Tools for debug:
- * print_free: print info about free blocks in free list
- * print_all: print all blocks
- */
-static void print_free() {
-    printf("----------------------------------------------------\n");
-    printf("Free block list:\n");
-    printf("%10s %5s %8s %8s\n", "addr", "size", "prev", "next");
-    char *bp;
-    for (bp = GET_BLOCK(mm_heap_start); bp != NULL; bp = GET_SUCC(bp)) {
-        printf("%p %5ld %p %p\n", bp, GET_SIZE(GET_HEAD(bp)), GET_PRED(bp),
-               GET_SUCC(bp));
-    }
-    printf("----------------------------------------------------\n");
-}
-
-static void print_all() {
-    printf("----------------------------------------------------\n");
-    printf("All blocks:\n%10s %5s %5s %5s\n", "addr", "size", "alloc",
-           "prevalloc");
-    char *bp = GET_BLOCK(mm_heap_start);
-    while (1) {
-        size_t size = GET_SIZE(GET_HEAD(bp));
-        printf("%p %5ld %5lx %5lx\n", bp, size, GET_ALLOC(GET_HEAD(bp)),
-               GET_ALLOC_PREV(GET_HEAD(bp)));
-        bp = GET_NEXT(bp);
-        if (!size)
-            break;
-    }
-    printf("----------------------------------------------------\n");
-}
-
 // Given block ptr bp, insert it into free block list
-static void free_list_insert(char *bp) {
-    char *first_bp = GET_BLOCK(mm_heap_start);
-    char *second_bp = GET_SUCC(first_bp);
-    PUT(GET_SUCC_P(bp), char *, second_bp);
-    PUT(GET_PRED_P(bp), char *, first_bp);
+static void free_list_insert(void *bp) {
+    void *first_bp = GET_BLOCK(mm_heap_start);
+    void *second_bp = GET_SUCC(first_bp);
+    PUT(GET_SUCC_P(bp), void *, second_bp);
+    PUT(GET_PRED_P(bp), void *, first_bp);
     if (second_bp != NULL)
-        PUT(GET_PRED_P(second_bp), char *, bp);
+        PUT(GET_PRED_P(second_bp), void *, bp);
     if (first_bp != NULL)
-        PUT(GET_SUCC_P(first_bp), char *, bp);
+        PUT(GET_SUCC_P(first_bp), void *, bp);
 }
 
 // Given free block ptr bp, remove it from free block list
-static void free_list_remove(char *bp) {
-    char *predecessor = GET_PRED(bp);
-    char *successor = GET_SUCC(bp);
+static void free_list_remove(void *bp) {
+    void *predecessor = GET_PRED(bp);
+    void *successor = GET_SUCC(bp);
     if (predecessor != NULL)
-        PUT(GET_SUCC_P(predecessor), char *, successor);
+        PUT(GET_SUCC_P(predecessor), void *, successor);
     if (successor != NULL)
-        PUT(GET_PRED_P(successor), char *, predecessor);
+        PUT(GET_PRED_P(successor), void *, predecessor);
 }
 
 // Coalesce adjoint free blocks with @bp, and return the free block after
 // coalescing.
-static void *coalesce(char *bp) {
+static void *coalesce(void *bp) {
     // Coalesce with previous block
     while (!GET_ALLOC_PREV(GET_HEAD(bp))) {
         free_list_remove(bp);  // remove bp from the free list
@@ -213,7 +183,7 @@ static void *coalesce(char *bp) {
     }
     // Coalesce with next block
     while (!GET_ALLOC(GET_HEAD(GET_NEXT(bp)))) {
-        char *next_bp = GET_NEXT(bp);
+        void *next_bp = GET_NEXT(bp);
         free_list_remove(next_bp);
 
         size_t new_size = GET_SIZE(GET_HEAD(next_bp)) + GET_SIZE(GET_HEAD(bp));
@@ -226,9 +196,9 @@ static void *coalesce(char *bp) {
 
 // Extend heap and return the last block after extending
 static void *extend_heap(size_t size) {
-    char *bp = mem_sbrk(size);
+    void *bp = mem_sbrk(size);
     if (bp == (void *)-1) {
-        return NULL;
+        DEBUG_RETURN(NULL);
     }
     // preserve the allocated bit for previous block
     int prev_alloc = GET_ALLOC_PREV(GET_HEAD(bp));
@@ -242,24 +212,21 @@ static void *extend_heap(size_t size) {
 }
 
 // Split free block, and insert the remind part into free list
-static void split(char *bp, size_t size) {
+static void split(void *bp, size_t size) {
     if (GET_SIZE(GET_HEAD(bp)) < size + MINIMUM_BLOCK_SIZE) {
-        return;
+        DEBUG_RETURN();
     }
     size_t old_size = GET_SIZE(GET_HEAD(bp));
     UPDATE_SIZE(GET_HEAD(bp), size);
     // create new footer
     PUT(GET_FOOT(bp), size_t, PACK(size, 0, 0));
 
-    char *next_bp = GET_NEXT(bp);
+    void *next_bp = GET_NEXT(bp);
     PUT(GET_HEAD(next_bp), size_t, PACK(old_size - size, 0, 0));
     PUT(GET_FOOT(next_bp), size_t, PACK(old_size - size, 0, 0));
 
     free_list_insert(next_bp);
-
-#ifdef DEBUG
-    mm_check_heap(__func__, __LINE__, VERBOSE);
-#endif
+    DEBUG_RETURN();
 }
 
 /*
@@ -271,21 +238,18 @@ int mm_init(void) {
 
     mem_sbrk(MINIMUM_BLOCK_SIZE + SIZE_T_SIZE);
     if (mem_sbrk == (void *)-1) {
-        return -1;
+        DEBUG_RETURN(-1);
     }
 
-    char *bp = GET_BLOCK(mm_heap_start);
+    void *bp = GET_BLOCK(mm_heap_start);
     // prologue block
     PUT(GET_HEAD(bp), size_t, PACK(MINIMUM_BLOCK_SIZE, 1, 1));
     PUT(GET_FOOT(bp), size_t, PACK(MINIMUM_BLOCK_SIZE, 1, 0));
-    PUT(GET_PRED_P(bp), char *, NULL);
-    PUT(GET_SUCC_P(bp), char *, NULL);
+    PUT(GET_PRED_P(bp), void *, NULL);
+    PUT(GET_SUCC_P(bp), void *, NULL);
     // epilogue header
     PUT(GET_HEAD(GET_NEXT(bp)), size_t, PACK(0, 1, 1));
-#ifdef DEBUG
-    mm_check_heap(__func__, __LINE__, VERBOSE);
-#endif
-    return 0;
+    DEBUG_RETURN(0);
 }
 
 /*
@@ -293,12 +257,35 @@ int mm_init(void) {
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 void *mm_malloc(size_t size) {
-    char *free_block = GET_BLOCK(mm_heap_start);
+    void *free_block = GET_BLOCK(mm_heap_start);
     // TODO: abs
     size_t real_size = REAL_SIZE(size);
     if (real_size < MINIMUM_BLOCK_SIZE) {
         real_size = MINIMUM_BLOCK_SIZE;
     }
+#ifdef BEST_FIT
+    void *best_free_block = NULL;
+    size_t best_size_diff = 0x7fffffff;
+    while (GET_SUCC(free_block) != NULL) {
+        free_block = GET_SUCC(free_block);
+        if (GET_SIZE(GET_HEAD(free_block)) >= real_size) {
+            size_t diff;
+            if ((diff = GET_SIZE(GET_HEAD(free_block)) - real_size) <
+                best_size_diff) {
+                best_free_block = free_block;
+                best_size_diff = diff;
+            }
+        }
+    }
+    if (best_free_block) {
+        split(best_free_block, real_size);
+        UPDATE_ALLOC(GET_HEAD(best_free_block), 1);
+        UPDATE_ALLOC_PREV(GET_HEAD(GET_NEXT(best_free_block)), 1);
+        free_list_remove(best_free_block);
+        DEBUG_RETURN(best_free_block);
+    }
+#else
+    // first  fit
     while (GET_SUCC(free_block) != NULL) {
         free_block = GET_SUCC(free_block);
         if (GET_SIZE(GET_HEAD(free_block)) >= real_size) {
@@ -309,12 +296,12 @@ void *mm_malloc(size_t size) {
             DEBUG_RETURN(free_block);
         }
     }
+#endif  // best fit
 
     // need to extend heap
     free_block = extend_heap(real_size);
     if (free_block == NULL) {
-        // failed to extend heap
-        return NULL;
+        DEBUG_RETURN(NULL);
     }
 
     free_list_remove(free_block);
@@ -370,16 +357,49 @@ void *mm_realloc(void *ptr, size_t size) {
     size_t copySize = old_size;
 
     newptr = mm_malloc(size);
-    if (newptr == NULL)
-        return NULL;
-    if (size < copySize)
-        copySize = size;
+    if (newptr == NULL) {
+        DEBUG_RETURN(NULL);
+    }
 
+    if (size < copySize) {
+        copySize = size;
+    }
     memcpy(newptr, ptr, copySize);
     mm_free(ptr);
     DEBUG_RETURN(newptr);
 }
 
+/* print_free: print info about free blocks in free list */
+static void print_free() {
+    printf("----------------------------------------------------\n");
+    printf("Free block list:\n");
+    printf("%10s %5s %8s %8s\n", "addr", "size", "prev", "next");
+    void *bp;
+    for (bp = GET_BLOCK(mm_heap_start); bp != NULL; bp = GET_SUCC(bp)) {
+        printf("%p %5ld %p %p\n", bp, GET_SIZE(GET_HEAD(bp)), GET_PRED(bp),
+               GET_SUCC(bp));
+    }
+    printf("----------------------------------------------------\n");
+}
+
+/* print all blocks */
+static void print_all() {
+    printf("----------------------------------------------------\n");
+    printf("All blocks:\n%10s %5s %5s %5s\n", "addr", "size", "alloc",
+           "prevalloc");
+    void *bp = GET_BLOCK(mm_heap_start);
+    while (1) {
+        size_t size = GET_SIZE(GET_HEAD(bp));
+        printf("%p %5ld %5lx %5lx\n", bp, size, GET_ALLOC(GET_HEAD(bp)),
+               GET_ALLOC_PREV(GET_HEAD(bp)));
+        bp = GET_NEXT(bp);
+        if (!size)
+            break;
+    }
+    printf("----------------------------------------------------\n");
+}
+
+/* Check if the block is correct */
 static void check_block(void *bp, const char *func, int line) {
     if ((size_t)bp % ALIGNMENT) {
         printf("Error: %p is not aligned.Func: %s,line: %d\n", bp, func, line);
@@ -394,6 +414,7 @@ static void check_block(void *bp, const char *func, int line) {
     }
 }
 
+/* Check the heap */
 void mm_check_heap(const char *func, int line, int verbose) {
     // check prologue header
     if (verbose) {
@@ -401,7 +422,7 @@ void mm_check_heap(const char *func, int line, int verbose) {
         print_free();
     }
 
-    char *bp = GET_BLOCK(mm_heap_start);
+    void *bp = GET_BLOCK(mm_heap_start);
     if (GET_SIZE(GET_HEAD(bp)) != MINIMUM_BLOCK_SIZE ||
         !GET_ALLOC(GET_HEAD(bp)) || !GET_ALLOC_PREV(GET_HEAD(bp))) {
         printf("Error: bad prologue header.Func: %s,line: %d\n", func, line);
