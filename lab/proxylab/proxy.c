8f11e272d9f2d5b2ce125e327d13eccc148852c3
yangtau <yanggtau+github@gmail.com>
2019-11-23 18:54:55 +0800

    format c files

--- lab/proxylab/proxy.c
+++ lab/proxylab/proxy.c
@@ -1,14 +1,25 @@
+#include "csapp.h"
 #include <stdio.h>
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
-/* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *user_agent_hdr =
+    "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
+    "Firefox/10.0.3\r\n";
 
-int main()
-{
-    printf("%s", user_agent_hdr);
-    return 0;
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Usage: %s port\n", argv[0]);
+  }
+
+  int listenfd = Open_listenfd(argv[1]);
+
+  while (1) {
+    int connfd = Accept(listenfd, NULL, NULL);
+    Close(connfd);
+  }
+  return 0;
 }
